6c6e3da53f934bd3a61b8549727321addc16b88b
Josiah Hunsinger <josiah.hunsinger@opswat.com>
2023-07-23 15:38:17 -0400

    Try to get input from user in a slightly different way

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ fi
 
 # Check if we should install the custom config
 if [ -e ~/.config/nvim ]; then
-    read -p 'It looks like config files alread exist at ~/.config/nvim. Would you like to overwrite them? [yN]' OVERWRITE_CONFIG
+    read -p 'It looks like config files alread exist at ~/.config/nvim. Would you like to overwrite them? [yN]' OVERWRITE_CONFIG < $(tty)
     if [ "$(echo $OVERWRITE_CONFIG | tr 'A-Z' 'a-z')" != "y" ]; then # Using negative logic intentionally to impliment default
         echo "Will not overwrite config. Exiting."
         exit
@@ -96,7 +96,7 @@ cd ~/.config
 # Try cloning via SSH, and prompt for HTTPS clone if that fails
 git clone git@github.com:aiguy110/nvim-config nvim
 if [ $? -ne 0 ]; then
-    read -p "Issue cloning using SSH. Clone using HTTPS? [Yn]" HTTP_CLONE
+    read -p 'Issue cloning using SSH. Clone using HTTPS? [Yn]' HTTP_CLONE < $(tty)
     if [ "$(echo "$HTTP_CLONE" | tr 'A-Z' 'a-z')" != "n" ]; then
         git clone https://github.com/aiguy110/nvim-config nvim
     else
