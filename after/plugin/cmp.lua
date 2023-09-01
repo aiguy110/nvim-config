0201960db8b404354c126dbae1e07b717a601412
Josiah Hunsinger <josiah.hunsinger@opswat.com>
2023-09-01 10:37:35 -0400

    Don't show "Text" completetions from LSPs

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,4 +1,10 @@
 local cmp = require("cmp")
+local function no_text(entry, _)
+    local kind = require"cmp.types".lsp.CompletionItemKind[entry:get_kind()]
+    return kind ~= "Text"
+end
+
+
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
@@ -21,9 +27,9 @@ cmp.setup({
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
-        { name = "vsnip" }, -- For vsnip users.
+        { name = "nvim_lsp", entry_filter = no_text },
+        { name = "nvim_lsp_signature_help", entry_filter = no_text },
+        { name = "vsnip", entry_filter = no_text }, -- For vsnip users.
         -- { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
